35bb4c29fa3d1dd5faf49528927230bb0ccd4e40
PotatoMeme <kshk0897@gmail.com>
2023-08-03 16:37:15 +0900

    [FEAT] : ⚙️ Create New Activity && Link Activity

--- Introduce App/app/src/main/java/com/potatomeme/itroduceapp/SignInActivity.kt
+++ Introduce App/app/src/main/java/com/potatomeme/itroduceapp/SignInActivity.kt
@@ -1,11 +1,26 @@
 package com.potatomeme.itroduceapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignInActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        findViewById<Button>(R.id.button_login).setOnClickListener {
+            //로그인 로직
+            val intent : Intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.button_sign_up).setOnClickListener {
+            //회원가입 로직수행
+            val intent : Intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
