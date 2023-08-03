29c80a10238e2b3e8aa0933cee77289a13f74226
PotatoMeme <kshk0897@gmail.com>
2023-08-03 19:08:41 +0900

    [FEAT] : ⚙️ home Activity basic logic

--- Introduce App/app/src/main/java/com/potatomeme/itroduceapp/HomeActivity.kt
+++ Introduce App/app/src/main/java/com/potatomeme/itroduceapp/HomeActivity.kt
@@ -2,10 +2,22 @@ package com.potatomeme.itroduceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val id = intent.getStringExtra(Key.INTENT_KEY_ID)
+        findViewById<TextView>(R.id.text_view_id).append(" : $id")
+        findViewById<TextView>(R.id.text_view_name).append(" : 김성환")
+
+        findViewById<Button>(R.id.button_exit).setOnClickListener {
+            finish()
+        }
     }
 }
