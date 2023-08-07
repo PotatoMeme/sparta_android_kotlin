d5ae938e7f811a3119774f58584d1a81e617097e
PotatoMeme <kshk0897@gmail.com>
2023-08-07 20:50:28 +0900

    [FEAT] : ⚙️ tabbar and viewpager and sample recyclerview

--- TodoApp/app/src/main/java/com/potatomeme/todoapp/MainActivity.kt
+++ TodoApp/app/src/main/java/com/potatomeme/todoapp/MainActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private val tabTitleArray: Array<String> = arrayOf<String>("Todo", "Bookmark")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +17,15 @@ class MainActivity : AppCompatActivity() {
         //toolbar
         val toolbarTemplate: Toolbar = findViewById<Toolbar>(R.id.main_toolbar)
         setSupportActionBar(toolbarTemplate)
+
+        //tabLayout
+        val viewPager: ViewPager2 = findViewById<ViewPager2>(R.id.main_viewpager)
+        val tabLayout: TabLayout = findViewById<TabLayout>(R.id.main_tab)
+
+        viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = tabTitleArray[position]
+        }.attach()
     }
 }
