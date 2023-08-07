1e1406a3e7a720366ac9c5502a5dfdcf61756e29
PotatoMeme <kshk0897@gmail.com>
2023-08-07 20:06:39 +0900

    [FEAT] : ⚙️ main toolbar && basic layout settings

--- TodoApp/app/src/main/java/com/potatomeme/todoapp/MainActivity.kt
+++ TodoApp/app/src/main/java/com/potatomeme/todoapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.potatomeme.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //toolbar
+        val toolbarTemplate: Toolbar = findViewById<Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(toolbarTemplate)
     }
 }
