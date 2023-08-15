5490e5017b0a1b497937bf72acdde0bec34a19da
PotatoMeme <kshk0897@gmail.com>
2023-08-15 14:48:43 +0900

    [FEAT] : ⚙️ floatingActionButton show and hide

--- TodoApp/app/src/main/java/com/potatomeme/todoapp/main/MainActivity.kt
+++ TodoApp/app/src/main/java/com/potatomeme/todoapp/main/MainActivity.kt
@@ -2,13 +2,15 @@ package com.potatomeme.todoapp.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.potatomeme.todoapp.adapter.ViewPagerAdapter
 import com.potatomeme.todoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     private val viewPagerAdapter by lazy {
         ViewPagerAdapter(this)
@@ -30,5 +32,22 @@ class MainActivity : AppCompatActivity() {
         TabLayoutMediator(mainTab, mainViewpager) { tab, position ->
             tab.setText(viewPagerAdapter.getTitle(position))
         }.attach()
+
+        mainViewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                when (position) {
+                    0 -> {
+                        floatingActionButton.show()
+                        //floatingActionButton.visibility = View.VISIBLE
+                    }
+
+                    else -> {
+                        floatingActionButton.hide()
+                        //floatingActionButton.visibility = View.GONE
+                    }
+                }
+            }
+        })
     }
 }
