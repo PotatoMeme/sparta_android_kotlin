c9c657d73e5ad69c6be484b7444f9776dd27048f
PotatoMeme <kshk0897@gmail.com>
2023-08-30 15:17:11 +0900

    [DESIGN] : 🎨 detail activity 구성

---
+++ AppleMarket/app/src/main/java/com/team8/applemarket/DetailActivity.kt
@@ -0,0 +1,21 @@
+package com.team8.applemarket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.team8.applemarket.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() = with(binding) {
+
+    }
+}
