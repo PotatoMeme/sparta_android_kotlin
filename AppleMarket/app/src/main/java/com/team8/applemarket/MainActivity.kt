fe55b7fabd75b6f3e330b0551bd530ee0f89ac5c
PotatoMeme <kshk0897@gmail.com>
2023-08-30 15:49:18 +0900

    [FEAT] : ⚙️ detail activity 데이터 주입

--- AppleMarket/app/src/main/java/com/team8/applemarket/MainActivity.kt
+++ AppleMarket/app/src/main/java/com/team8/applemarket/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.team8.applemarket
 
 import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -18,12 +22,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityMainBinding
+
+    private val activityResultLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { activityResult: ActivityResult ->
+        if (activityResult.resultCode == RESULT_OK) {
+
+        }
+    }
     private val recyclerViewAdapter: ItemRecyclerViewAdapter by lazy {
         ItemRecyclerViewAdapter(SampleData.itemArr) { item: Item, user: User ->
             //todo activity 이동관련 로직 구현!!
             Log.d(TAG, "RecyclerViewItem($item) Clicked")
+            activityResultLauncher.launch(DetailActivity.newIntent(this, item, user))
         }
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
