a4c1fd062a64552dfee93584ecfc9a787726171e
PotatoMeme <kshk0897@gmail.com>
2023-09-24 14:39:16 +0900

    [INIT] : 😀 프로잭트 생성 및 기본 setting
    기존 TodoApp을 다시짜면서 복기

---
+++ TodoAppAnotherType/app/src/main/java/com/potatomeme/todoappanothertype/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.potatomeme.todoappanothertype.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.potatomeme.todoappanothertype.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
